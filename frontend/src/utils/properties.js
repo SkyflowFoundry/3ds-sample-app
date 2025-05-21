ab0d2baca6e0655da8e2d3de5c49c95ddeedc767
Shashank prajapati <shashank-prajapathi@skyflow.com>
2025-05-21 13:11:15 +0530

    FINT-792:Updated properties.js

--- frontend/src/utils/properties.js
+++ frontend/src/utils/properties.js
@@ -2,7 +2,7 @@ const properties = {
   PORT: process.env.PORT || 8000,
   VAULT_ID: process.env.VAULT_ID || "",
   VAULT_URL: process.env.VAULT_URL || "",
-  BEFFE_URL: process.env.BEFFE_URL || "",
+  BACKEND_URL: process.env.BACKEND_URL || "",
   ORIGIN: process.env.ORIGIN || "",
 };
 
